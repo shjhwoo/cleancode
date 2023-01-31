2170332bd0b5b8c97cc0bb17aa8f5e70ec81c0dd
suxxzzy <suxxzzy@github.com>
2023-01-31 22:58:32 +0900

    나: 단계 쪼개기

--- ch6/6-11-2.js
+++ ch6/6-11-2.js
@@ -1,18 +1,35 @@
-import fs from 'fs';
+import fs from "fs";
 
-if (!process.argv[2]) {
-  throw new Error('파일 이름을 입력하세요');
+//refact!
+
+function checkFileName() {
+  if (!process.argv[2]) {
+    throw new Error("파일 이름을 입력하세요");
+  }
 }
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
-  throw new Error('파일이 존재하지 않습니다');
+function checkFileExist() {
+  const fileName = `./${process.argv[2]}.json`;
+  if (!fs.existsSync(fileName)) {
+    throw new Error("파일이 존재하지 않습니다");
+  }
+  return fileName;
 }
 
-const rawData = fs.readFileSync(fileName);
-const orders = JSON.parse(rawData);
-if (process.argv.includes('-r')) {
-  console.log(orders.filter((order) => order.status === 'ready').length);
-} else {
-  console.log(orders.length);
+function readFileRawContent(fileName) {
+  const rawData = fs.readFileSync(fileName);
+  return JSON.parse(rawData);
 }
+
+function printProductCount(orders) {
+  if (process.argv.includes("-r")) {
+    console.log(orders.filter((order) => order.status === "ready").length);
+  } else {
+    console.log(orders.length);
+  }
+}
+
+checkFileName();
+const fileName = checkFileExist();
+const orders = readFileRawContent(fileName);
+printProductCount(orders);
