3d1bfcbd48bafdc86255b8ecde6f3901c5aa78f9
suxxzzy <suxxzzy@github.com>
2023-02-06 21:04:44 +0900

    기본형을 객체로: 나

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -1,15 +1,15 @@
 export class Order {
-  constructor(data) {
-    this.priority = data.priority;
+  constructor(priority) {
+    this.priority = priority;
+  }
+
+  isHighPriority() {
+    return this.priority === "high" || this.priority === "rush";
   }
 }
 
-const orders = [
-  new Order({ priority: 'normal' }),
-  new Order({ priority: 'high' }),
-  new Order({ priority: 'rush' }),
-];
+const orders = [new Order("normal"), new Order("high"), new Order("rush")];
 
-const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
-).length;
+const highPriorityCount = orders.filter((o) => o.isHighPriority()).length;
+
+console.log(highPriorityCount);
