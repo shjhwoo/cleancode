989e353472e26b5154f5d2336b419f8f8507f14c
suxxzzy <suxxzzy@github.com>
2023-02-07 20:50:06 +0900

    임시변수를 질의함수로

--- ch7/7-4.js
+++ ch7/7-4.js
@@ -5,11 +5,16 @@ class Order {
     this.#quantity = quantity;
     this.#item = item;
   }
+  //getter를 사용해서, 임시 변수를 질의함수로 바꾸면 된다.
+  get basePrice() {
+    return this.#quantity * this.#item.price;
+  }
+
+  get discountFactor() {
+    return this.basePrice > 1000 ? 0.98 : 0.95;
+  }
 
   get price() {
-    const basePrice = this.#quantity * this.#item.price;
-    const discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    return this.basePrice * this.discountFactor;
   }
 }
