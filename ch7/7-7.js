7592f720e9fc23937d224ce1895823ac286a1cda
suxxzzy <suxxzzy@github.com>
2023-02-08 20:31:08 +0900

    위임 숨기기: 나

--- ch7/7-7.js
+++ ch7/7-7.js
@@ -1,38 +1,15 @@
 class Person {
   #name;
-  #department;
-  constructor(name, department) {
-    this.#name = name;
-    this.#department = department;
-  }
-
-  get name() {
-    return this.#name;
-  }
-
-  get department() {
-    return this.#department;
-  }
-
-  set department(arg) {
-    this.#department = arg;
-  }
-}
-
-export class Department {
   #manager;
   #chargeCode;
-  constructor(manager, chargeCode) {
+  constructor(name, manager, chargeCode) {
+    this.#name = name;
     this.#manager = manager;
     this.#chargeCode = chargeCode;
   }
 
-  get chargeCode() {
-    return this.#chargeCode;
-  }
-
-  set chargeCode(arg) {
-    this.#chargeCode = arg;
+  get name() {
+    return this.#name;
   }
 
   get manager() {
@@ -42,9 +19,17 @@ export class Department {
   set manager(arg) {
     this.#manager = arg;
   }
+
+  get chargeCode() {
+    return this.#chargeCode;
+  }
+
+  set chargeCode(arg) {
+    this.#chargeCode = arg;
+  }
 }
 
-const person = new Person('Tom', new Department('aManager', '999'));
+const person = new Person("Tom", "aManager", "999");
 console.log(person.name);
-console.log(person.department.manager);
-console.log(person.department.chargeCode);
+console.log(person.manager);
+console.log(person.chargeCode);
