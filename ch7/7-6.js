e977eb392e502bcd966b9784a508697b9295bc56
suxxzzy <suxxzzy@github.com>
2023-02-08 20:24:33 +0900

    클래스 인라인하기

--- ch7/7-6.js
+++ ch7/7-6.js
@@ -22,32 +22,33 @@ export class TrackingInformation {
     this.#trackingNumber = arg;
   }
 
-  get display() {
+  get trackingInfo() {
     return `${this.shippingCompany}: ${this.trackingNumber}`;
   }
 }
 
-export class Shipment {
-  #trackingInformation;
-  constructor(trackingInformation) {
-    this.#trackingInformation = trackingInformation;
-  }
+// export class Shipment {
+//   #trackingInformation;
+//   constructor(trackingInformation) {
+//     this.#trackingInformation = trackingInformation;
+//   }
 
-  get trackingInfo() {
-    return this.#trackingInformation.display;
-  }
+//   get trackingInfo() {
+//     return this.#trackingInformation.display;
+//   }
 
-  get trackingInformation() {
-    return this.#trackingInformation;
-  }
+//   get trackingInformation() {
+//     return this.#trackingInformation;
+//   }
 
-  set trackingInformation(trackingInformation) {
-    this.#trackingInformation = trackingInformation;
-  }
-}
+//   set trackingInformation(trackingInformation) {
+//     this.#trackingInformation = trackingInformation;
+//   }
+// }
 
-const shipment = new Shipment(new TrackingInformation(999, 'Maersk'));
+//const shipment = new Shipment(new TrackingInformation(999, "Maersk"));
+const shipment = new TrackingInformation(999, "Maersk");
 console.log(shipment.trackingInfo);
 
-shipment.trackingInformation.shippingCompany = 'COSCO';
+shipment.shippingCompany = "COSCO";
 console.log(shipment.trackingInfo);
