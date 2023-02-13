e7618afaeba51f5e5fe3625f169873c11c1e5b29
suxxzzy <suxxzzy@github.com>
2023-02-13 21:06:47 +0900

    응집도: 엘리, 함수

--- ch8/8-1-1.js
+++ ch8/8-1-1.js
@@ -1,42 +1,12 @@
 export function trackSummary(points) {
   const totalTime = calculateTime();
-  const totalDistance = calculateDistance();
+  const totalDistance = calculateDistance(points);
   const pace = totalTime / 60 / totalDistance;
   return {
     time: totalTime,
     distance: totalDistance,
     pace: pace,
   };
-
-  function calculateDistance() {
-    let result = 0;
-    for (let i = 1; i < points.length; i++) {
-      result += distance(points[i - 1], points[i]);
-    }
-    return result;
-  }
-
-  function distance(p1, p2) {
-    // 포뮬라: http://www.movable-type.co.uk/scripts/latlong.html
-    const EARTH_RADIUS = 3959; // in miles
-    const dLat = radians(p2.lat) - radians(p1.lat);
-    const dLon = radians(p2.lon) - radians(p1.lon);
-    const a =
-      Math.pow(Math.sin(dLat / 2), 2) +
-      Math.cos(radians(p2.lat)) *
-        Math.cos(radians(p1.lat)) *
-        Math.pow(Math.sin(dLon / 2), 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return EARTH_RADIUS * c;
-  }
-
-  function radians(degrees) {
-    return (degrees * Math.PI) / 180;
-  }
-
-  function calculateTime() {
-    return 10000;
-  }
 }
 
 const newYork = {
@@ -52,63 +22,32 @@ const tokyo = {
 const summary = trackSummary([newYork, tokyo]);
 console.log(summary);
 
-class Track {
-  #points;
-  #time;
-  #EARTH_RADIUS;
-  constructor(points) {
-    this.#points = [...points];
-    this.#time = 10000;
-    this.#EARTH_RADIUS = 3959;
+function calculateDistance(points) {
+  let result = 0;
+  for (let i = 1; i < points.length; i++) {
+    result += distance(points[i - 1], points[i]);
   }
+  return result;
+}
 
-  get Time() {
-    return this.#time;
-  }
+function distance(p1, p2) {
+  // 포뮬라: http://www.movable-type.co.uk/scripts/latlong.html
+  const EARTH_RADIUS = 3959; // in miles
+  const dLat = radians(p2.lat) - radians(p1.lat);
+  const dLon = radians(p2.lon) - radians(p1.lon);
+  const a =
+    Math.pow(Math.sin(dLat / 2), 2) +
+    Math.cos(radians(p2.lat)) *
+      Math.cos(radians(p1.lat)) *
+      Math.pow(Math.sin(dLon / 2), 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return EARTH_RADIUS * c;
+}
 
-  getSummary() {
-    return {
-      time: this.#time,
-      distance: this.calculateDistance(),
-      pace: this.getPace(),
-    };
-  }
+function radians(degrees) {
+  return (degrees * Math.PI) / 180;
+}
 
-  calculateDistance() {
-    //응집도가 높은가??.. 정말??
-    let result = 0;
-    for (let i = 1; i < this.#points.length; i++) {
-      result += this.getDistance(this.#points[i - 1], this.#points[i]);
-    }
-    return result;
-  }
-
-  getPace() {
-    return this.#time / 60 / totalDistance;
-  }
-
-  getDistance(p1, p2) {
-    //해당 배열 내 있는지 유효성 검사할것
-    const dLat = this.getLat(p1, p2);
-    const dLon = this.getLon(p1, p2);
-    const a =
-      Math.pow(Math.sin(dLat / 2), 2) +
-      Math.cos(this.getRadians(p1.lat)) *
-        Math.cos(this.getRadians(p2.lat)) *
-        Math.pow(Math.sin(dLon / 2), 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return this.#EARTH_RADIUS * c;
-  }
-
-  getLat(p1, p2) {
-    return this.getRadians(p1.lat) - this.getRadians(p2.lat);
-  }
-
-  getLon(p1, p2) {
-    return this.getRadians(p1.lon) - this.getRadians(p2.lon);
-  }
-
-  getRadians(degrees) {
-    return (degrees * Math.PI) / 180;
-  }
+function calculateTime() {
+  return 10000;
 }
