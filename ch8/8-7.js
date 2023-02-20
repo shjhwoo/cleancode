417d51e6fbc6c9f316f2b489eb444f5ff43b9fb6
suxxzzy <suxxzzy@github.com>
2023-02-20 21:36:17 +0900

    반복문 쪼개기

--- ch8/8-7.js
+++ ch8/8-7.js
@@ -8,3 +8,20 @@ export function reportYoungestAgeAndTotalSalary(people) {
 
   return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
 }
+
+//개선
+export function reportYoungestAgeAndTotalSalary2(people) {
+  if (people.length === 0) return "no data";
+
+  return `youngestAge: ${getYoungest(
+    people.map((p) => p.age)
+  )}, totalSalary: ${getTotalSalary(people.map((p) => p.salary))}`;
+}
+
+function getYoungest(peopleAges) {
+  return peopleAges.length > 0 ? Math.min(peopleAges) : Infinity;
+}
+
+function getTotalSalary(peopleSalaries) {
+  return peopleSalaries.length > 0 ? peopleSalaries.reduce((a, b) => a + b) : 0;
+}
