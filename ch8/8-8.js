b8c9431b0586bda522b58150a429c1b5ea85e0ae
suxxzzy <suxxzzy@github.com>
2023-02-21 21:13:33 +0900

    나: 반복문 파이프라인으로 만들기

--- ch8/8-8.js
+++ ch8/8-8.js
@@ -1,15 +1,15 @@
 export function acquireData(input) {
-  const lines = input.split('\n');
-  let firstLine = true;
+  const lines = input.split("\n");
+  //let firstLine = true;
   const result = [];
   for (const line of lines) {
-    if (firstLine) {
-      firstLine = false;
-      continue;
-    }
-    if (line.trim() === '') continue;
-    const record = line.split(',');
-    if (record[1].trim() === 'India') {
+    // if (firstLine) {
+    //   firstLine = false;
+    //   continue;
+    // }
+    if (line.trim() === "") continue;
+    const record = line.split(",");
+    if (record[1].trim() === "India") {
       result.push({ city: record[0].trim(), phone: record[2].trim() });
     }
   }
@@ -24,3 +24,14 @@ Porto Alegre, Brazil, +55 51 3079 3550\n
 Chennai, India, +91 44 660 44766`;
 const result = acquireData(input);
 console.log(result);
+
+function acquireData(input) {
+  const lines = input.split("\n").slice(1);
+  const india = lines.filter((line) => isIndia(line)).split(",");
+
+  return [{ city: india[0].trim(), phone: india[2].trim() }];
+}
+
+function isIndia(line) {
+  return line.split(",")[1].trim() === "India";
+}
