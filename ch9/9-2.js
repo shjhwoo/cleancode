0c543e87a9a4878b2f5105fbdcbedf7bd227d3bb
suxxzzy <suxxzzy@github.com>
2023-02-23 21:39:57 +0900

    필드 이름 바꾸기

--- ch9/9-2.js
+++ ch9/9-2.js
@@ -1,13 +1,13 @@
 class Organization {
   constructor(data) {
-    this._name = data.name;
+    this._organizationName = data.organizationName;
     this._country = data.country;
   }
-  get name() {
-    return this._name;
+  get organizationName() {
+    return this._organizationName;
   }
-  set name(value) {
-    this._name = value;
+  set organizationName(value) {
+    this._organizationName = value;
   }
   get country() {
     return this._country;
@@ -16,7 +16,8 @@ class Organization {
     this._country = value;
   }
 }
+
 const organization = new Organization({
-  name: '드림코딩',
-  country: '대한민국',
+  organizationName: "드림코딩",
+  country: "대한민국",
 });
