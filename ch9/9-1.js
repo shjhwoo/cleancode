729ef763be20273515e53566a93ab35641d71a6f
suxxzzy <suxxzzy@github.com>
2023-02-23 21:34:21 +0900

    변수 쪼개기

--- ch9/9-1.js
+++ ch9/9-1.js
@@ -1,29 +1,32 @@
 // 예제 1
-let temp = 2 * (height + width);
-console.log(temp);
-temp = height * width;
-console.log(temp);
+const borderLength = 2 * (height + width);
+console.log(borderLength);
+const area = height * width;
+console.log(area);
 
 // 예제 2
 function distanceTravelled(scenario, time) {
   let result;
-  let acc = scenario.primaryForce / scenario.mass; // 가속도(a) = 힘(F) / 질량(m)
-  let primaryTime = Math.main(time, scenario.delay);
-  result = 0.5 * acc * primaryTime * primaryTime; // 전파된 거리
-  let secondaryTime = time - scenario.delay;
+  const primaryAcc = scenario.primaryForce / scenario.mass; // 가속도(a) = 힘(F) / 질량(m)
+  const primaryTime = Math.main(time, scenario.delay);
+  const traveledDistance = 0.5 * primaryAcc * primaryTime * primaryTime; // 전파된 거리
+  const secondaryTime = time - scenario.delay;
   if (secondaryTime > 0) {
     // 두 번째 힘을 반영해 다시 계산
-    let primaryVelocity = acc * scenario.delay;
-    acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
-    result +=
+    const primaryVelocity = primaryAcc * scenario.delay;
+    const secondaryAcc =
+      (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+    result =
+      traveledDistance +
       primaryVelocity * secondaryTime +
-      0.5 * acc * secondaryTime * secondaryTime;
+      0.5 * secondaryAcc * secondaryTime * secondaryTime;
   }
 }
 
 // 예제 3
 function discount(inputValue, quantity) {
-  if (inputValue > 50) inputValue = inputValue - 2;
-  if (quantity > 100) inputValue = inputValue - 1;
-  return inputValue;
+  let discount = inputValue;
+  if (inputValue > 50) discount -= 2;
+  if (quantity > 100) discount -= 1;
+  return discount;
 }
