31b26f9a0a608a6019bdd727d7c2a24896482005
suxxzzy <suxxzzy@github.com>
2023-02-24 21:46:42 +0900

    값을 참조로 바꾸기

--- ch9/9-5.js
+++ ch9/9-5.js
@@ -1,11 +1,22 @@
+const customerRepository = new CustomerRepository();
+
+const order = new Order(
+  data.number,
+  customerRepository.registerCustomer(data.customerId)
+);
+
 class Order {
-  constructor(data) {
-    this._number = data.number;
-    this._customer = new Customer(data.customerId);
+  constructor(number, customer) {
+    this._number = number;
+    this._customer = customer;
   }
 
   get customer() {
     return this._customer;
+  }
+
+  set customer(id) {
+    this._customer._id = id;
   }
 }
 
@@ -16,5 +27,28 @@ class Customer {
 
   get id() {
     return this._id;
+  }
+
+  set id(value) {
+    this._id = value;
+  }
+}
+
+class CustomerRepository {
+  #customers;
+
+  constructor() {
+    this.#customers = new Map();
+  }
+
+  registerCustomer(id) {
+    if (!this.#customers.has(id)) {
+      this.#customers.set(id, new Customer(id));
+    }
+    return this.findCustomer(id);
+  }
+
+  findCustomer(id) {
+    return this.#customers.get(id);
   }
 }
