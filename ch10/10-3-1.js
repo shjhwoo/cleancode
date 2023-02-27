f2cfdf8c44df70082dcdca3f44122a20a0fb733f
suxxzzy <suxxzzy@github.com>
2023-02-27 21:12:37 +0900

    조건식 중첩을 보호구문으로 바꾸기

--- ch10/10-3-1.js
+++ ch10/10-3-1.js
@@ -1,22 +1,18 @@
 export function payAmount(employee) {
-  let result;
   if (employee.isSeparated) {
-    result = { amount: 0, reasonCode: 'SEP' };
-  } else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: 'RET' };
-    } else {
-      // lorem.ipsum(dolor.sitAmet);
-      // consectetur(adipiscing).elit();
-      // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      // ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-      return result;
-    }
+    return { amount: 0, reasonCode: "SEP" };
   }
-  return result;
+
+  if (employee.isRetired) {
+    return { amount: 0, reasonCode: "RET" };
+  }
+  // lorem.ipsum(dolor.sitAmet);
+  // consectetur(adipiscing).elit();
+  // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+  // ut.enim.ad(minim.veniam);
+  return someFinalComputation();
 }
 
 function someFinalComputation() {
-  return { amount: 999, reasonCode: 'UNICORN' };
+  return { amount: 999, reasonCode: "UNICORN" };
 }
--- ch10/10-3-2.js
+++ ch10/10-3-2.js
@@ -1,11 +1,25 @@
-export function adjustedCapital(instrument) {
-  let result = 0;
-  if (instrument.capital > 0) {
-    if (instrument.interestRate > 0 && instrument.duration > 0) {
-      result =
-        (instrument.income / instrument.duration) *
-        anInstrument.adjustmentFactor;
-    }
+// export function adjustedCapital(instrument) {
+//   let result = 0;
+//   if (instrument.capital > 0) {
+//     if (instrument.interestRate > 0 && instrument.duration > 0) {
+//       result =
+//         (instrument.income / instrument.duration) *
+//         anInstrument.adjustmentFactor;
+//     }
+//   }
+//   return result;
+// }
+
+export function adjustCapital(instrument) {
+  if (instrument.capital <= 0) {
+    return 0;
   }
-  return result;
+
+  if (instrument.interestRate > 0 && instrument.duration > 0) {
+    return (
+      (instrument.income / instrument.duration) * anInstrument.adjustmentFactor
+    );
+  }
 }
+
+// 또는 조건식을 함수화해도 된다.
