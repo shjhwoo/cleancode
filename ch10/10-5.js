b3aabbc6f39f167f2db49a766f72860f8d01a981
suxxzzy <suxxzzy@github.com>
2023-03-06 21:50:52 +0900

    특이 케이스 추가하기

--- ch10/10-5.js
+++ ch10/10-5.js
@@ -1,10 +1,52 @@
+class Hotel {
+  constructor() {
+    this.rooms = [];
+  }
+
+  addRoom(roomNumber) {
+    this.rooms[roomNumber](new Room(roomNumber));
+  }
+
+  emptyRoom(roomNumber) {
+    this.rooms[roomNumber] = new emptyRoom(roomNumber); //null 원시값보단 특이케이스 넣자 null 또는 unknown 나타내는 객체를 쓰자.
+  }
+
+  cleanRooms() {
+    this.rooms.forEach((room) => room.clean());
+  }
+}
+
+class Room {
+  constructor(roomNumber) {
+    this.roomNumber = roomNumber;
+  }
+
+  clean() {
+    console.log(this.roomNumber + "청소");
+  }
+}
+
+class emptyRoom extends Room {
+  clean() {
+    console.log("방 비었어.");
+  }
+}
+
 export class Site {
   constructor(customer) {
     this._customer = customer;
   }
 
   get customer() {
-    return this._customer;
+    return this._customer === "unknown"
+      ? new unknownCustomer()
+      : new Customer(this._customer);
+  }
+}
+
+class unknownCustomer extends Customer {
+  get name() {
+    return "occupant";
   }
 }
 
@@ -35,8 +77,7 @@ export function customerName(site) {
   const aCustomer = site.customer;
   // 더 많은 코드가 여기에 있음
   let customerName;
-  if (aCustomer === 'unknown') customerName = 'occupant';
-  else customerName = aCustomer.name;
+  customerName = aCustomer.name;
 
   return customerName;
 }
