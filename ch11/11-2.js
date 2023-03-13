1b9b2aca8386ae1eb23d2094a5e6fe5c2eaab924
suxxzzy <suxxzzy@github.com>
2023-03-13 21:35:55 +0900

    함수 매개변수화하기

--- ch11/11-2.js
+++ ch11/11-2.js
@@ -7,30 +7,33 @@ function fivePercentRaise(person) {
   person.salary = person.salary.multiply(1.05);
 }
 
+//개선 후
+function raisePersonalSalary(person, factor) {
+  person.salary = person.salary.multiply(raise + factor);
+}
+
 // 예제 2
 export function baseCharge(usage) {
   if (usage < 0) return usd(0);
   const amount =
-    bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+    withinBand(usage, 0, 100) * 0.03 +
+    withinBand(usage, 100, 200) * 0.05 +
+    withinBand(usage, 200, Infinity) * 0.07;
   return usd(amount);
 }
 
-function bottomBand(usage) {
-  return Math.min(usage, 100);
+function withinBand(usage, start, end) {
+  return usage > start ? Math.min(usage, end) - start : 0;
 }
 
-function middleBand(usage) {
-  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
-}
-
-function topBand(usage) {
+function withinBand(usage) {
   return usage > 200 ? usage - 200 : 0;
 }
 
 function usd(value) {
   return {
-    currency: '$',
-    currencyName: 'USD',
+    currency: "$",
+    currencyName: "USD",
     value: value,
   };
 }
