8ac548285a4d5bc74719dd9e6308c4884142824c
suxxzzy <suxxzzy@github.com>
2023-03-15 22:08:14 +0900

    매개변수를 질의함수로 바꾸기

--- ch11/11-5.js
+++ ch11/11-5.js
@@ -4,20 +4,23 @@ export class Order {
     this.itemPrice = itemPrice;
   }
 
-  get finalPrice() {
-    const basePrice = this.quantity * this.itemPrice;
-    let discountLevel;
-    if (this.quantity > 100) discountLevel = 2;
-    else discountLevel = 1;
-    return this.discountedPrice(basePrice, discountLevel);
+  get discountLevel() {
+    return this.quantity > 100 ? 2 : 1;
   }
 
-  discountedPrice(basePrice, discountLevel) {
-    switch (discountLevel) {
-      case 1:
-        return basePrice * 0.95;
-      case 2:
-        return basePrice * 0.9;
-    }
+  get discountRate() {
+    return this.discountLevel === 1 ? 0.95 : 0.9;
+  }
+
+  get finalPrice() {
+    return this.discountedPrice();
+  }
+
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+
+  discountedPrice() {
+    return this.basePrice * this.discountRate;
   }
 }
