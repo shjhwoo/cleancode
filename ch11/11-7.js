8a3eea55e2a7f63308723639686b2ac0fb6c1de5
suxxzzy <suxxzzy@github.com>
2023-03-17 22:00:16 +0900

    세터 제거

--- ch11/11-7.js
+++ ch11/11-7.js
@@ -1,4 +1,13 @@
 class Person {
-  get name() {}
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+  get id() {
+    return this.id;
+  }
+  get name() {
+    return this.name;
+  }
   set name(value) {}
 }
