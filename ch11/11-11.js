e9b1f6e9eb50939a8950a4cb081e7c9f2136f370
suxxzzy <suxxzzy@github.com>
2023-03-21 22:06:00 +0900

    수정된 값 반환하기

--- ch11/11-11.js
+++ ch11/11-11.js
@@ -1,13 +1,16 @@
 export function ascentVelocity(points, totalMinutes) {
   function calculateAscent() {
+    let totalAscent = 0;
+
     for (let i = 1; i < points.length; i++) {
       const verticalChange = points[i].elevation - points[i - 1].elevation;
       totalAscent += verticalChange > 0 ? verticalChange : 0;
     }
+
+    return totalAscent;
   }
 
-  let totalAscent = 0;
-  calculateAscent([{ elevation: 10 }, { elevation: 20 }]);
+  const totalAscent = calculateAscent([{ elevation: 10 }, { elevation: 20 }]);
 
   return totalAscent / totalMinutes;
 }
