27d58a716b3262d8c57b8f8192aa01bc05b05028
suxxzzy <suxxzzy@github.com>
2023-03-21 23:12:06 +0900

    오류 코드를 예외로 바꾸기

--- ch11/11-12.js
+++ ch11/11-12.js
@@ -2,3 +2,25 @@ function localShippingRules(data) {
   if (data) return new ShippingRules(data);
   else return -23;
 }
+
+//프로그래밍에서 제공해주는 에러 클래스를 사용하는 것이 좋다
+
+function localShippingRules2(data) {
+  if (data) return new ShippingRules();
+  else throw new OrderProcessingError(-23);
+}
+
+class OrderProcessingError extends Error {
+  constructor(errorCode) {
+    super();
+    this.errorCode = errorCode;
+  }
+}
+
+try {
+  const result = localShippingRules2();
+} catch (e) {
+  if (e instanceof OrderProcessingError) {
+    console.log(e);
+  }
+}
