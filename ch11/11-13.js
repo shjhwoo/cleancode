be59c6ce092e5c98afa97dd8f40611a6b9ff3680
suxxzzy <suxxzzy@github.com>
2023-03-22 23:32:28 +0900

    예외를 사전확인으로 바꾸기

--- ch11/11-13.js
+++ ch11/11-13.js
@@ -2,13 +2,17 @@ const values = [];
 function getValueForPeriod(periodNumber) {
   const value = values[periodNumber];
   if (!value) {
-    throw new Error('value is undefined');
+    throw new Error("value is undefined");
   }
   return value;
 }
 
 try {
-  getValueForPeriod(-10);
+  let target = -10;
+  if (!values[target]) {
+    throw new Error("target is under zero");
+  }
+  getValueForPeriod(target);
 } catch (error) {
-  console.log('에러 발생!');
+  console.log("에러 발생!");
 }
