38ea8c1da4f578c44999706133b837b815df926e
suxxzzy <suxxzzy@github.com>
2023-03-23 21:47:25 +0900

    메서드 올리기

--- ch12/12-1.js
+++ ch12/12-1.js
@@ -1,24 +1,38 @@
 // 예시 1
-class Employee {}
+class Employee {
+  constructor(name) {
+    this._name = name;
+  }
 
-class Salesperson extends Employee {
-  get name() {}
-}
-
-class Engineer extends Employee {
-  get name() {}
-}
-
-// 예시 2
-class Party {}
-
-class Department extends Party {
-  get totalAnnualCost() {
-    return this.monthlyCost * 12;
+  get name() {
+    return this._name;
   }
 }
-class Employee extends Party {
+
+class Salesperson extends Employee {}
+
+class Engineer extends Employee {}
+
+const s1 = new Salesperson("haha");
+const e1 = new Engineer("ee");
+
+console.log("s1:", s1.name, "e1:", e1.name);
+
+// // 예시 2
+class Party {
+  constructor(monthlyCost) {
+    this._monthlyCost = monthlyCost;
+  }
+
   get annualCost() {
-    return this.monthlyCost * 12;
+    return this._monthlyCost * 12;
   }
 }
+
+class Department extends Party {}
+class Employee2 extends Party {}
+
+const d1 = new Department("300");
+const e2 = new Employee2("789");
+
+console.log("d1:", d1.annualCost, "e2:", e2.annualCost);
