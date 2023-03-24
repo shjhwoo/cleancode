ed770a9751f993ad14caeea3c14fbe88cb142ade
suxxzzy <suxxzzy@github.com>
2023-03-24 22:09:43 +0900

    타입 코드는 서브클래스로 바꾸기

--- ch12/12-6.js
+++ ch12/12-6.js
@@ -1,26 +1,59 @@
 class Employee {
   #name;
-  #type;
-  constructor(name, type) {
-    this.validateType(type);
+  constructor(name) {
     this.#name = name;
-    this.#type = type;
   }
 
-  validateType(arg) {
-    if (!['engineer', 'manager', 'salesperson'].includes(arg)) {
-      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
-    }
+  get name() {
+    return this.#name;
   }
 
   get type() {
-    return this.#type;
+    return "employee";
   }
 
   toString() {
     return `${this.#name} (${this.type})`;
   }
+
+  createEmployee(name, type) {
+    switch (type) {
+      case "engineer":
+        return new Engineer(name);
+      case "manager":
+        return new Manager(name);
+      default:
+        throw new Error("무효");
+    }
+  }
 }
 
-const ellie = new Employee('엘리', 'engineer');
-const bob = new Employee('밥', 'manager');
+class Engineer extends Employee {
+  constructor(name) {
+    super(name);
+    this.type = "engineer";
+  }
+
+  get name() {
+    return this.#name;
+  }
+}
+
+class Manager extends Employee {
+  constructor(name) {
+    super(name);
+    this.type = "manager";
+  }
+
+  get name() {
+    return this.#name;
+  }
+}
+
+const ellie = new Engineer("엘리");
+const bob = new Manager("밥");
+
+//타입 코드를 두는 것보다는 서브클래스를 만들어 정리하는 것이 좋다.
+//생성자에 로직을 두는 대신 팩터리 메서드에 로직을 두자
+
+//검증코드 없이도 작동하도록, 서브클래스를 만드는 게 나음.
