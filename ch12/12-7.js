31b533022b7e36ffe1ec4f8dc54cdbfa20995f73
suxxzzy <suxxzzy@github.com>
2023-03-27 21:11:28 +0900

    서브클래스 제거

--- ch12/12-7.js
+++ ch12/12-7.js
@@ -1,53 +1,32 @@
 class Person {
   #name;
-  constructor(name) {
+  constructor(name, gender) {
     this.#name = name;
+    this.#gender = gender;
   }
 
   get name() {
     return this.#name;
   }
 
-  get genderCode() {
-    return 'X';
-  }
-}
-
-class Male extends Person {
-  get genderCode() {
-    return 'M';
-  }
-}
-
-class Female extends Person {
-  get genderCode() {
-    return 'F';
+  get gender() {
+    return this.#gender;
   }
 }
 
 function loadFromInput(data) {
   const result = [];
   data.forEach((record) => {
-    let person;
-    switch (record.gender) {
-      case 'M':
-        person = new Male(record.name);
-        break;
-      case 'F':
-        person = new Female(record.name);
-        break;
-      default:
-        person = new Person(record.name);
-    }
+    const person = new Person(record.name, record.gender);
     result.push(person);
   });
   return result;
 }
 
 const people = loadFromInput([
-  { name: '엘리', gender: 'F' },
-  { name: '철수', gender: 'M' },
-  { name: '밥', gender: 'M' },
+  { name: "엘리", gender: "F" },
+  { name: "철수", gender: "M" },
+  { name: "밥", gender: "M" },
 ]);
-const numberOfMales = people.filter((p) => p instanceof Male).length;
+const numberOfMales = people.filter((p) => p.gender === "M").length;
 console.log(numberOfMales);
