2744eb32bc49eed8e2506ced0f213c26008ece2d
suxxzzy <suxxzzy@github.com>
2023-03-29 22:09:08 +0900

    슈퍼클래스를 위임으로 바꾸기

--- ch12/12-11-1.js
+++ ch12/12-11-1.js
@@ -1,3 +1,9 @@
 class List {}
 
 class Stack extends List {}
+
+class Stack {
+  constructor() {
+    this.List = new List();
+  }
+}
--- ch12/12-11-2.js
+++ ch12/12-11-2.js
@@ -18,14 +18,16 @@ class CatalogItem {
   }
 }
 
-class Scroll extends CatalogItem {
-  constructor(id, title, tags, dataLastCleaned) {
-    super(id, title, tags);
+class Scroll {
+  constructor(id, CatalogItem, dataLastCleaned) {
+    this.id = id; //두루마리 문서, 즉 카탈로그의 사본이며 구분자로 id를 둠. 상속보단 위임이 적절한 경우이다.
+    this.CatalogItem = CatalogItem;
+    //책에서처럼 카탈로그 클래스를 통채로 주지 말고, 필요한 부분만 함수 인수로 전달해주자.
     this._lastCleaned = dataLastCleaned;
   }
 
   needsCleaning(targetDate) {
-    const threshold = this.hasTag('revered') ? 700 : 1500;
+    const threshold = this.hasTag("revered") ? 700 : 1500;
 
     return this.daysSinceLastCleaning(targetDate) > threshold;
   }
